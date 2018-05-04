c08f8fc4456c5b012821b61f696a1b60293ae349
David <david.raese@gmx.de>
2018-05-04 14:54:33 +0200

    initial commit: serverSetup

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require("express");
+
+const app = express();
+const PORT = 3000;
+
+app.get("/", (req, res) => {
+  res.send("Erster Versuch Express zu benutzen");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is on Port ${PORT}`);
+});
