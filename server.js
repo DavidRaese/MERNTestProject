99361dfe8d41fbcfbf75d9a4896c052813326824
David <david.raese@gmx.de>
2018-05-05 15:41:11 +0200

    enabeling cookies

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const cookieSession = require("cookie-session");
+const passport = require("passport");
 require("./models/User");
 require("./services/passport");
 const authRoutes = require("./routes/authRoutes");
@@ -7,6 +9,16 @@ const keys = require("./config/keys");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+//middleware
+app.use(
+  cookieSession({
+    maxAge: 30 * 24 * 60 * 60 * 1000,
+    keys: [keys.cookieKey]
+  })
+);
+app.use(passport.initialize());
+app.use(passport.session());
 
 //connection with mongoDB
 mongoose.connect(keys.mongoURI);
--- services/passport.js
+++ services/passport.js
@@ -7,6 +7,16 @@ const keys = require("../config/keys");
 //gets user model from mongodb, this is importent for testing
 const User = mongoose.model("users");
 
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+
+passport.deserializeUser((id, done) => {
+  User.findById(id).then(user => {
+    done(null, user);
+  });
+});
+
 passport.use(
   new GoogleStrategy(
     {
@@ -17,11 +27,12 @@ passport.use(
     (accsesToken, refreshtoken, profile, done) => {
       User.findOne({ googleID: profile.id }).then(existingUser => {
         if (existingUser) {
+          done(null, existingUser);
         } else {
           //1. create modelinstance
           const newUser = new User({ googleID: profile.id });
           //2. save modelinstance to mongodb
-          newUser.save();
+          newUser.save().then(user => done(null, user));
         }
       });
     }
